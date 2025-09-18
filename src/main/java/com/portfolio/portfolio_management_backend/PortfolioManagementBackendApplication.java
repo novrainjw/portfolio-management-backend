e92fffdd52ace6860c59eb9e049da022b217d188
Johnny Jia <novrain.jw@gmail.com>
2025-09-18 14:49:05 -0300

    Initial commit

---
+++ src/main/java/com/portfolio/portfolio_management_backend/PortfolioManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portfolio/portfolio_management_backend/PortfolioManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
